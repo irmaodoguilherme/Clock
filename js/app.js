28e516397ce14dbed16318eb11db2f8c454d1997
irmaodoguilherme <airesguilherme83@gmail.com>
2024-02-24 18:05:43 -0300

    new clock code

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const clockContainer = document.querySelector('[data-js="clockContainer"]')
+
+const oneSecond = 1000
+
+const concatenateZero = unit => unit < 10 ? `0${unit}` : unit
+
+const formatTimeUnits = units => units.map(concatenateZero)
+
+const getClockHTML = ([hours, minutes, seconds]) => `
+  <span class="bg-dark bg-opacity-75">${hours}</span> :
+  <span class="bg-dark bg-opacity-75">${minutes}</span> :
+  <span class="bg-dark bg-opacity-75">${seconds}</span>
+`
+
+const getTime = () => {
+  const present = new Date()
+  const hours = present.getHours()
+  const minutes = present.getMinutes()
+  const seconds = present.getSeconds()
+
+  return [hours, minutes, seconds]
+}
+
+const updateClock = () => {
+  const [hours, minutes, seconds] = getTime()
+  const formattedTime = formatTimeUnits([hours, minutes, seconds])
+
+  clockContainer.innerHTML = getClockHTML(formattedTime)
+}
+
+setInterval(() => updateClock(), oneSecond)
